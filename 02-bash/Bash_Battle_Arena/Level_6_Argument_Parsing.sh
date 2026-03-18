d61c2b20744e4690f904797aff0a3eefc884497d
AbdullahHAbdi <abdullah.husseinabdi@gmail.com>
2026-03-18 12:28:58 -0400

    Added solution to Bash Battle Arena Level 6

---
+++ 02-bash/Bash_Battle_Arena/Level_6_Argument_Parsing.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Mission: Write a script that accepts a filename as an argument and prints the number of lines in that file. If no filename is provided, display a message saying 'No file provided'.
+
+file_contents() {
+    filename=$1
+
+    if [ -z "$filename" ]; then
+        echo "No file provided."
+    else
+        wc -l < "$filename"
+    fi 
+}
+
